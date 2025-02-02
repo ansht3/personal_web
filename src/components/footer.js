556e3f1281d4e2177d52764d63bd7090ad9bf258
Ansh2 <ansh.nynj@gmail.com>
2025-02-01 20:40:32 -0800

    further changes

--- src/components/footer.js
+++ src/components/footer.js
@@ -106,7 +106,7 @@ const Footer = () => {
 
       <StyledCredit tabindex="-1">
         <a href="https://github.com/anshtandon2/">
-          <div>Built by Ansh Tandon.</div>
+          <div>Built by Ansh Tandon</div>
 
           {githubInfo.stars && githubInfo.forks && (
             <div className="github-stats">
