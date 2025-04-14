7643f6ce5ea978841418d19ceb1f05cdcc737a2e
Ansh Tandon <ansh.nynj@gmail.com>
2025-04-13 19:45:45 -0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ module.exports = {
   socialMedia: [
     {
       name: 'GitHub',
-      url: 'https://github.com/AnshTandon2',
+      url: 'https://github.com/ansht3',
     },
     {
       name: 'Instagram',
