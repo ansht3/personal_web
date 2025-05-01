499f5a04b406304ceba8b9f40090a9152b217fb4
Ansh Tandon <ansh.nynj@gmail.com>
2025-05-01 12:31:16 -0400

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
   siteMetadata: {
     title: 'Ansh Tandon',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
+      'Software Engineer specializing in full-stack development, machine learning, and systems.',
     siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
     image: '/og2.0.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@bchiang7',
