94be4f690ede1068a2a5e440010949e10b7b410e
Ansh Tandon <ansh.nynj@gmail.com>
2025-06-30 16:22:22 -0500

    Update logo.js

--- src/components/icons/logo.js
+++ src/components/icons/logo.js
@@ -1,30 +1,31 @@
 import React from 'react';
 
 const IconLogo = () => (
-  <svg id="logo" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 84 96">
-    <title>Logo</title>
-    <g transform="translate(-8.000000, -2.000000)">
-      <g transform="translate(11.000000, 5.000000)">
-        <polygon
-          id="Shape"
-          stroke="currentColor"
-          strokeWidth="5"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          points="39 0 0 22 0 67 39 90 78 68 78 23"
-        />
-        {/* Updated path for the letter A */}
-        <path
-          d="M26,60 L39,30 L52,60 L45.5,45 L32.5,45"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="5"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-      </g>
-    </g>
-  </svg>
+  <p>anshtandon</p>
+  // <svg id="logo" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 84 96">
+  //   <title>Logo</title>
+  //   <g transform="translate(-8.000000, -2.000000)">
+  //     <g transform="translate(11.000000, 5.000000)">
+  //       <polygon
+  //         id="Shape"
+  //         stroke="currentColor"
+  //         strokeWidth="5"
+  //         strokeLinecap="round"
+  //         strokeLinejoin="round"
+  //         points="39 0 0 22 0 67 39 90 78 68 78 23"
+  //       />
+  //       {/* Updated path for the letter A */}
+  //       <path
+  //         d="M26,60 L39,30 L52,60 L45.5,45 L32.5,45"
+  //         fill="none"
+  //         stroke="currentColor"
+  //         strokeWidth="5"
+  //         strokeLinecap="round"
+  //         strokeLinejoin="round"
+  //       />
+  //     </g>
+  //   </g>
+  // </svg>
 );
 
 export default IconLogo;
