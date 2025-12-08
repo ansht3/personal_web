1d23d3e8b536959ef9374e2882b8b89922ec095c
Ansh Tandon <ansh.nynj@gmail.com>
2025-12-08 14:12:40 -0500

    Add Google Analytics page view tracking

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,3 +3,14 @@
  *
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
+
+export const onRouteUpdate = ({ location }) => {
+  if (typeof window.gtag === 'function') {
+    window.gtag('event', 'page_view', {
+      page_location: window.location.href,
+      page_path: location.pathname,
+      page_title: document.title,
+      page_referrer: document.referrer
+    });
+  }
+};
